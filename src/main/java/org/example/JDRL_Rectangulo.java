c0cc790e8904cb32da06db3475f6808a8f7cd184
jorgedlr709 <jorgedlr709@gmail.com>
2025-04-20 20:47:04 +0200

    JDLR_Rectangulo

---
+++ src/main/java/org/example/JDRL_Rectangulo.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class JDRL_Rectangulo {
+    private int id;
+    private double ladoA;
+    private double ladoB;
+    private String color;
+
+    public JDRL_Rectangulo() {
+    }
+
+    public JDRL_Rectangulo(int id, double ladoA, double ladoB, String color) {
+        this.id = id;
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "JDRL_Rectangulo{" +
+                "id=" + id +
+                ", ladoA=" + ladoA +
+                ", ladoB=" + ladoB +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
